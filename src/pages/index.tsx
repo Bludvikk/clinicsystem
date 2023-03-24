23fa6882fd68992419a37b40528539d4ce3d83ef
Bludvikk <cleverdmasoy@gmail.com>
2023-03-24 11:24:17 +0800

    boshet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,40 @@
-import { useSession } from "next-auth/react"
+// const IndexPage: NextPage = () => {
+//   const router = useRouter();
+//   const { data: session, status } = useSession()
 
-export default function Component() {
-  const { data: session, status } = useSession()
+//   if (status === "authenticated")
+//     return router.push('/dashboard')
 
-  if (status === "authenticated") {
-    return <p>Signed in as {session.user.username}</p>
-  }
+//   return <a href="/login">Sign in</a>
+// }
 
-  return <a href="/login">Sign in</a>
-}
+// Page.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>;
+
+// export default Page;
+
+import { NextPage } from "next";
+import BlankLayout from "@/@core/layouts/BlankLayout";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect } from "react";
+import Spinner from "src/@core/components/spinner";
+
+const IndexPage: NextPage = () => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  const redirect = () => {
+    if (status === "authenticated") return router.push("/dashboard");
+    else return router.push("/login");
+  };
+
+  useEffect(() => {
+    redirect();
+  }, []);
+
+  return <Spinner />;
+};
+
+IndexPage.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>;
+
+export default IndexPage;
