555fd9112a20ab42f3630d0ad9b3f4bf39d0ab7e
reymondgomera <reymondgomera24@gmail.com>
2023-04-08 11:31:39 +0800

    augment/extends next-auth types

---
+++ types/next-auth.d.ts
@@ -0,0 +1,21 @@
+import { UsersType } from "@/utils/db.type";
+import NextAuth from "next-auth";
+
+export type AuthenticatedUserType = Pick<
+  UsersType,
+  "id" | "email" | "userName" | "role" | "department" | "status"
+>;
+
+declare module "next-auth" {
+  interface Session {
+    user: AuthenticatedUserType;
+  }
+
+  interface User extends AuthenticatedUserType {}
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    user: AuthenticatedUserType;
+  }
+}
