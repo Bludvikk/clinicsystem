4eb548f61a0cc9bd1d1d64a8b641a4b57979a541
reymondgomera <reymondgomera24@gmail.com>
2023-04-29 10:21:43 +0800

    mofidy to cater dynamic filter and add date filters functionality

--- src/utils/form.component.tsx
+++ src/utils/form.component.tsx
@@ -1,4 +1,4 @@
-import { getDependencyData } from "@/server/hooks/reference";
+import { getDependencyData } from '@/server/hooks/reference';
 import {
   InputLabel,
   Select,
@@ -15,18 +15,17 @@ import {
   GridProps,
   Grid,
   ListItemTextProps,
-} from "@mui/material";
-import FormHelperText from "@mui/material/FormHelperText";
-import { DatePicker } from "@mui/x-date-pickers";
-import {
-  Controller,
-  ControllerFieldState,
-  ControllerRenderProps,
-  FieldErrors,
-} from "react-hook-form";
-import { ExtendedPropsType, FormControlPropsType } from "./common.type";
-import { getFilterObjValue } from "./helper";
-import { ChangeEvent } from "react";
+  TextFieldProps,
+  BoxProps
+} from '@mui/material';
+import FormHelperText from '@mui/material/FormHelperText';
+import { Controller, ControllerFieldState, ControllerRenderProps, FieldErrors } from 'react-hook-form';
+import { ExtendedPropsType, FormControlPropsType } from './common.type';
+import { getFilterObjValue } from './helper';
+import React, { ChangeEvent } from 'react';
+import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
+import DatePickerWrapper from '@/@core/styles/libs/react-datepicker';
+import moment from 'moment';
 
 type FormContextType<TUnion> = {
   objFieldProp: FormControlPropsType<TUnion>;
@@ -39,10 +38,7 @@ type FormContextType<TUnion> = {
 };
 
 export function FormObjectComponent<TUnion>(
-  props: Pick<
-    FormContextType<TUnion>,
-    "objFieldProp" | "control" | "errors" | "getValues" | "setValue"
-  >
+  props: Pick<FormContextType<TUnion>, 'objFieldProp' | 'control' | 'errors' | 'getValues' | 'setValue'>
 ) {
   const { objFieldProp, control, errors, getValues, setValue } = props;
 
@@ -58,7 +54,7 @@ export function FormObjectComponent<TUnion>(
           field,
           fieldState,
           getValues,
-          setValue,
+          setValue
         })
       }
     />
@@ -67,8 +63,7 @@ export function FormObjectComponent<TUnion>(
   return (
     <>
       {objControl}
-      {!objFieldProp.disabledErrors &&
-        getFormErrorMessage({ errors, dbField: objFieldProp.dbField })}
+      {!objFieldProp.disabledErrors && getFormErrorMessage({ errors, dbField: objFieldProp.dbField })}
     </>
   );
 }
@@ -76,24 +71,23 @@ export function FormObjectComponent<TUnion>(
 export function FormContextType<TUnion>(props: FormContextType<TUnion>) {
   const { objFieldProp, field, fieldState, getValues, setValue } = props;
   const {
+    boxAttribute,
     formControlAttribute,
     formControlLabelAttribute,
     textFieldAttribute,
     dropDownAttribute,
-    datePickerAttribute,
+    reactDatePickerAttribute,
     gridAttribute,
-    checkboxAttribute,
+    checkboxAttribute
   } = objFieldProp.extendedProps as ExtendedPropsType;
 
-  const label = objFieldProp.required
-    ? objFieldProp.label + "*"
-    : objFieldProp.label;
+  const label = objFieldProp.required ? objFieldProp.label + '*' : objFieldProp.label;
 
   switch (objFieldProp.type) {
-    case "dropDown":
+    case 'dropDown':
       return (
         <DropdownData
-          type="control"
+          type='control'
           id={objFieldProp.entityId!}
           objFieldProp={objFieldProp}
           field={field}
@@ -106,30 +100,31 @@ export function FormContextType<TUnion>(props: FormContextType<TUnion>) {
         />
       );
 
-    case "datePicker":
+    case 'datePicker':
       return (
-        <FormControl
-          {...(formControlAttribute
-            ? formControlAttribute
-            : { fullWidth: true })}
-        >
+        <DatePickerWrapper {...(boxAttribute ? boxAttribute : { width: '100%' })}>
           <DatePicker
-            {...field}
-            {...datePickerAttribute}
-            slotProps={{
-              textField: {
-                label,
-                placeholder: label,
-                autoFocus: objFieldProp.autoFocus,
-                error: Boolean(fieldState.error),
-                value: field.value ? field.value : null,
-              },
-            }}
+            {...reactDatePickerAttribute}
+            id={field.name}
+            popperPlacement='bottom-start'
+            selected={moment(field.value).isValid() ? field.value : null}
+            onChange={field.onChange}
+            placeholderText='MM/DD/YYYY'
+            isClearable
+            customInput={
+              <TextField
+                {...textFieldAttribute}
+                label={label}
+                fullWidth
+                autoFocus={objFieldProp.autoFocus}
+                error={Boolean(fieldState.error)}
+              />
+            }
           />
-        </FormControl>
+        </DatePickerWrapper>
       );
 
-    case "checkbox":
+    case 'checkbox':
       return (
         <FormControlLabel
           {...formControlLabelAttribute}
@@ -141,16 +136,16 @@ export function FormContextType<TUnion>(props: FormContextType<TUnion>) {
               id={field.name}
               autoFocus={objFieldProp.autoFocus}
               checked={Boolean(getValues(objFieldProp.dbField))}
-              sx={fieldState.error ? { color: "error.main" } : null}
+              sx={fieldState.error ? { color: 'error.main' } : null}
             />
           }
           sx={{
-            "& .MuiFormControlLabel-label": { color: "text.primary" },
+            '& .MuiFormControlLabel-label': { color: 'text.primary' }
           }}
         />
       );
 
-    case "multi-checkbox":
+    case 'multi-checkbox':
       return (
         <CheckBoxData
           id={objFieldProp.entityId!}
@@ -168,11 +163,7 @@ export function FormContextType<TUnion>(props: FormContextType<TUnion>) {
 
     default:
       return (
-        <FormControl
-          {...(formControlAttribute
-            ? formControlAttribute
-            : { fullWidth: true })}
-        >
+        <FormControl {...(formControlAttribute ? formControlAttribute : { fullWidth: true })}>
           <TextField
             {...field}
             {...textFieldAttribute}
@@ -181,7 +172,7 @@ export function FormContextType<TUnion>(props: FormContextType<TUnion>) {
             label={label}
             placeholder={label}
             error={!objFieldProp.disabledErrors && Boolean(fieldState.error)}
-            value={field.value ? field.value : ""}
+            value={field.value ? field.value : ''}
           />
         </FormControl>
       );
@@ -191,18 +182,20 @@ export function FormContextType<TUnion>(props: FormContextType<TUnion>) {
 export function getFormErrorMessage({ ...props }) {
   const { errors, dbField } = props;
 
-  return (
-    errors[dbField] && (
-      <FormHelperText sx={{ color: "error.main" }}>
-        {errors[dbField]?.message}
-      </FormHelperText>
-    )
-  );
+  const properties = dbField.split('.');
+
+  let currentError = errors;
+  for (let i = 0; i < properties.length; i++) {
+    currentError = currentError[properties[i]];
+    if (currentError === undefined) return null;
+  }
+
+  return currentError && <FormHelperText sx={{ color: 'error.main' }}>{currentError.message}</FormHelperText>;
 }
 
 type DropdownProps<TUnion> = {
   id: number;
-  type: "filter" | "control";
+  type: 'filter' | 'control';
   searchFilterValue: any;
   handleSearchFilter: (...event: any[]) => void;
   label?: string;
@@ -211,6 +204,7 @@ type DropdownProps<TUnion> = {
   fieldState?: ControllerFieldState;
   dropDownAttribute?: SelectProps;
   formControlAttribute?: FormControlProps;
+  customMenuItem?: { [key: string]: React.ReactNode };
 };
 
 export function DropdownData<TUnion>(props: DropdownProps<TUnion>) {
@@ -224,47 +218,35 @@ export function DropdownData<TUnion>(props: DropdownProps<TUnion>) {
     label,
     dropDownAttribute,
     formControlAttribute,
+    customMenuItem
   } = props;
 
   const { entities, references } = getDependencyData({ entities: [id] });
 
   if (!entities.data && !references.data) return null;
 
-  const dataLoaded =
-    !entities.isLoading &&
-    !!entities.data &&
-    !references.isLoading &&
-    !!references.data;
-  const entity = entities.data?.find((e) => e.id === id);
+  const dataLoaded = !entities.isLoading && !!entities.data && !references.isLoading && !!references.data;
+  const entity = entities.data?.find(e => e.id === id);
 
   if (!entity) return null;
 
   const currentLabel = label ? label : entity.name;
   let currentValue = 0;
 
-  if (type === "filter") {
+  if (type === 'filter') {
     const { dropDownValue } = getFilterObjValue(searchFilterValue);
-    currentValue =
-      dropDownValue && dropDownValue[entity.fieldProp]
-        ? dropDownValue[entity.fieldProp]
-        : 0;
+    currentValue = dropDownValue && dropDownValue[entity.fieldProp] ? dropDownValue[entity.fieldProp] : 0;
   }
 
-  if (type === "control") {
+  if (type === 'control') {
     currentValue = searchFilterValue ? searchFilterValue : 0;
   }
 
   return (
     <>
-      {dataLoaded && entity ? (
-        <FormControl
-          {...(formControlAttribute
-            ? formControlAttribute
-            : { fullWidth: true })}
-        >
-          <InputLabel id={`${entity.fieldProp}-select`}>
-            {currentLabel}
-          </InputLabel>
+      {dataLoaded && entity && entity.code !== 'timeframe' && (
+        <FormControl {...(formControlAttribute ? formControlAttribute : { fullWidth: true })}>
+          <InputLabel id={`${entity.fieldProp}-select`}>{currentLabel}</InputLabel>
           <Select
             {...(dropDownAttribute ? dropDownAttribute : { fullWidth: true })}
             id={`select-${entity.fieldProp}`}
@@ -277,23 +259,48 @@ export function DropdownData<TUnion>(props: DropdownProps<TUnion>) {
             value={currentValue}
           >
             <MenuItem value={0}>Select {currentLabel}</MenuItem>
-            {references.data
-              .filter((item) => item.isShow)
-              .map((item) => (
-                <MenuItem key={item.id} value={item.id}>
-                  {item.name}
-                </MenuItem>
-              ))}
+            {references.data.map(item => (
+              <MenuItem key={item.id} value={item.id}>
+                {item.name}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
-      ) : null}
+      )}
+      {dataLoaded && entity && entity.code === 'timeframe' && (
+        <FormControl {...(formControlAttribute ? formControlAttribute : { fullWidth: true })}>
+          <InputLabel id={`${entity.fieldProp}-select`}>{currentLabel}</InputLabel>
+          <Select
+            {...(dropDownAttribute ? dropDownAttribute : { fullWidth: true })}
+            id={`select-${entity.fieldProp}`}
+            name={entity.fieldProp}
+            label={currentLabel}
+            labelId={`${entity.fieldProp}-select`}
+            autoFocus={objFieldProp?.autoFocus}
+            error={fieldState && Boolean(fieldState.error)}
+            onChange={handleSearchFilter}
+            value={currentValue}
+          >
+            <MenuItem value={0}>Select {currentLabel}</MenuItem>
+            {references.data.map(item =>
+              customMenuItem && customMenuItem[item.code] ? (
+                <div key={item.id}>{customMenuItem[item.code]}</div>
+              ) : (
+                <MenuItem key={item.id} value={item.code}>
+                  {item.name}
+                </MenuItem>
+              )
+            )}
+          </Select>
+        </FormControl>
+      )}
     </>
   );
 }
 
 type MultiCheckboxProps<TUnion> = {
   id: number;
-  type?: "multi";
+  type?: 'multi';
   label?: string;
   objFieldProp?: FormControlPropsType<TUnion>;
   field?: ControllerRenderProps;
@@ -317,38 +324,25 @@ export function CheckBoxData<TUnion>(props: MultiCheckboxProps<TUnion>) {
     checkboxAttribute,
     getValues,
     setValue,
-    formControlLabelAttribute,
+    formControlLabelAttribute
   } = props;
 
   const { entities, references } = getDependencyData({ entities: [id] });
 
   if (!entities.data && !references.data) return null;
 
-  const dataLoaded =
-    !entities.isLoading &&
-    !!entities.data &&
-    !references.isLoading &&
-    !!references.data;
-  const entity = entities.data?.find((e) => e.id === id);
+  const dataLoaded = !entities.isLoading && !!entities.data && !references.isLoading && !!references.data;
+  const entity = entities.data?.find(e => e.id === id);
 
   if (!entity) return null;
 
-  const currentLabel = label ? label : entity.name;
-  let currentValue = 0;
-
   // works only for array of values
-  const handleCheckboxChange = (
-    e: ChangeEvent<HTMLInputElement>,
-    path: string
-  ) => {
+  const handleCheckboxChange = (e: ChangeEvent<HTMLInputElement>, path: string) => {
     const currentArrayValues = getValues(path);
 
-    if (e.target.checked)
-      setValue(path, [...currentArrayValues, parseInt(e.target.value)]);
+    if (e.target.checked) setValue(path, [...currentArrayValues, parseInt(e.target.value)]);
     else {
-      const filterValues = currentArrayValues.filter(
-        (value: any) => value !== parseInt(e.target.value)
-      );
+      const filterValues = currentArrayValues.filter((value: any) => value !== parseInt(e.target.value));
       setValue(path, [...filterValues]);
     }
   };
@@ -362,9 +356,7 @@ export function CheckBoxData<TUnion>(props: MultiCheckboxProps<TUnion>) {
             <Grid key={i} item {...gridAttribute}>
               <FormControlLabel
                 key={i}
-                {...(formControlLabelAttribute
-                  ? formControlLabelAttribute
-                  : { sx: { width: "100%" } })}
+                {...(formControlLabelAttribute ? formControlLabelAttribute : { sx: { width: '100%' } })}
                 label={item.name}
                 control={
                   <Checkbox
@@ -375,14 +367,12 @@ export function CheckBoxData<TUnion>(props: MultiCheckboxProps<TUnion>) {
                     name={objFieldProp?.dbField as string}
                     autoFocus={objFieldProp?.autoFocus}
                     checked={getValues(objFieldProp?.dbField).includes(item.id)}
-                    sx={fieldState?.error ? { color: "error.main" } : null}
-                    onChange={(e) =>
-                      handleCheckboxChange(e, objFieldProp?.dbField as string)
-                    }
+                    sx={fieldState?.error ? { color: 'error.main' } : null}
+                    onChange={e => handleCheckboxChange(e, objFieldProp?.dbField as string)}
                   />
                 }
                 sx={{
-                  "& .MuiFormControlLabel-label": { color: "text.primary" },
+                  '& .MuiFormControlLabel-label': { color: 'text.primary' }
                 }}
               />
             </Grid>
@@ -405,5 +395,59 @@ export function ListItemTextData(props: ListItemTextType) {
     <Grid item {...gridAttribute}>
       <ListItemText {...listItemTextAttribute} />
     </Grid>
+  );
+}
+
+type TextInputSearchProps = {
+  searchFilterValue: any;
+  handleSearchFilter: (...event: any[]) => void;
+  textFieldAttribute?: TextFieldProps;
+};
+
+export function TextInputSearch(props: TextInputSearchProps) {
+  const { handleSearchFilter, searchFilterValue, textFieldAttribute } = props;
+  const { inputValue } = getFilterObjValue(searchFilterValue);
+
+  return (
+    <TextField
+      {...(textFieldAttribute ? textFieldAttribute : { fullWidth: true })}
+      placeholder='Search'
+      name='inputValue'
+      onChange={handleSearchFilter}
+      value={inputValue ? inputValue : ''}
+    />
+  );
+}
+
+type DateRangeInputSearchProps = {
+  searchFilterValue: any;
+  handleDateRangeFilter: (...event: any[]) => void;
+  reactDatePickerAttribute?: Omit<ReactDatePickerProps, 'onChange'>;
+  boxAttribute?: BoxProps;
+  customInput: React.ReactNode;
+};
+
+export function DateRangeInputSearch(props: DateRangeInputSearchProps) {
+  const { handleDateRangeFilter, searchFilterValue, reactDatePickerAttribute, boxAttribute, customInput } = props;
+  const { dateRangeInputValue } = getFilterObjValue(searchFilterValue);
+  const { start, end } = dateRangeInputValue ? dateRangeInputValue : { start: null, end: null };
+
+  return (
+    <DatePickerWrapper {...(boxAttribute ? boxAttribute : { width: '100%' })}>
+      <DatePicker
+        {...reactDatePickerAttribute}
+        selectsRange
+        monthsShown={2}
+        name='dateRangeInputValue'
+        placeholderText='Date Range Search'
+        startDate={start}
+        endDate={end}
+        selected={start}
+        shouldCloseOnSelect={false}
+        onChange={handleDateRangeFilter}
+        isClearable
+        customInput={customInput}
+      />
+    </DatePickerWrapper>
   );
 }
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,26 +1,24 @@
-import { SelectChangeEvent } from "@mui/material";
-import { TRPCClientError } from "@trpc/client";
-import _ from "lodash";
-import { useState, useCallback } from "react";
-import {
-  DynamicType,
-  FilterQueryInputType,
-  FormInputType,
-} from "./common.type";
+import { SelectChangeEvent } from '@mui/material';
+import { TRPCClientError } from '@trpc/client';
+import _ from 'lodash';
+import { useState, useCallback, MutableRefObject } from 'react';
+import { DynamicType, FilterQueryInputType, FormInputType } from './common.type';
+import moment from 'moment';
+import ReactPDF from '@react-pdf/renderer';
 
 type ErrorType = {
-  status: "CONFLICT" | "ERROR";
+  status: 'CONFLICT' | 'ERROR';
   message: string;
 };
 
 export function errorUtil(error: unknown): ErrorType {
   if (error instanceof TRPCClientError) {
     if (error.data?.httpStatus === 409) {
-      return { status: "CONFLICT", message: error.message };
+      return { status: 'CONFLICT', message: error.message };
     }
   }
 
-  return { status: "ERROR", message: `Unexpected error: ${error}!` };
+  return { status: 'ERROR', message: `Unexpected error: ${error}!` };
 }
 
 export function getRandomNumber(min: number, max: number) {
@@ -33,54 +31,64 @@ export function useFilterControlChange() {
   const handleSearchFilter = useCallback((e: SelectChangeEvent) => {
     const { name, value } = e.target;
 
-    let key: FormInputType = "textField"; //e.type === change
+    let key: FormInputType = 'textField'; //e.type === change
     switch (e.type) {
-      case "click":
-        key = "dropDown";
+      case 'click':
+        key = 'dropDown';
         break;
 
       default:
-        key = "textField";
+        key = 'textField';
         break;
     }
 
-    setSearchFilter((prev) => {
+    setSearchFilter(prev => {
       prev[key] = { ...prev[key], [name]: value };
 
       return { ...prev };
     });
   }, []);
 
-  return { searchFilter, setSearchFilter, handleSearchFilter };
+  const handleDateRangeFilter = (date: Date[]) => {
+    setSearchFilter(prev => {
+      prev['dateRangeInputValue'] = { start: date[0], end: date[1] };
+
+      return { ...prev };
+    });
+  };
+
+  return { searchFilter, setSearchFilter, handleSearchFilter, handleDateRangeFilter };
 }
 
 export function getFilterObjValue(objVal?: FilterQueryInputType) {
-  const searchFilter = _.get(objVal, "searchFilter");
-  const dropDownValue = _.get(objVal, "dropDown");
-  const inputValue = _.get(objVal, "textField.inputValue");
+  const searchFilter = _.get(objVal, 'searchFilter');
+  const dropDownValue = _.get(objVal, 'dropDown');
+  const inputValue = _.get(objVal, 'textField.inputValue');
+  const dateRangeInputValue = _.get(objVal, 'dateRangeInputValue');
 
-  return { searchFilter, dropDownValue, inputValue };
+  return { searchFilter, dropDownValue, inputValue, dateRangeInputValue };
 }
 
 export function isObjEmpty(obj: any) {
   return _.isEmpty(obj);
 }
 
-export type NestedKeyOf<ObjectType extends object> = {
-  [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object
-    ? `${Key}` | `${Key}.${NestedKeyOf<ObjectType[Key]>}`
-    : `${Key}`;
-}[keyof ObjectType & (string | number)];
+export type NestedKey<O extends Record<string, unknown>> = {
+  [K in Extract<keyof O, string>]: O[K] extends Array<any>
+    ? K
+    : O[K] extends Record<string, unknown>
+    ? `${K}` | `${K}.${NestedKey<O[K]>}`
+    : K;
+}[Extract<keyof O, string>];
 
-// a reviver function checks if the parsed value is a string that matches the ISO 8601 date format, and if so, it constructs a new Date object from the parsed values.
-export const parseJSONWithDates = (jsonString: string): any => {
+/** 
+ * a reviver function checks if the parsed value is a string that matches the ISO 8601 date format, 
+   and if so, it constructs a new Date object from the parsed values. 
+*/
+export function parseJSONWithDates(jsonString: string): any {
   const reviver = (key: string, value: any) => {
-    if (typeof value === "string") {
-      // checks if the parsed value is a string that matches the ISO 8601 date format (e.g "2023-04-12T12:00:00.000Z")
-      const match =
-        /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})Z$/.exec(
-          value
-        );
+    if (typeof value === 'string') {
+      const match = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})Z$/.exec(value);
       if (match) {
         return new Date(value);
       }
@@ -89,4 +97,60 @@ export const parseJSONWithDates = (jsonString: string): any => {
   };
 
   return JSON.parse(jsonString, reviver);
+}
+
+export function checkDate(date: Date) {
+  const REFERENCE = moment();
+  const momentDate = moment(date);
+
+  return {
+    isToday() {
+      const TODAY = REFERENCE.clone().startOf('day');
+      return momentDate.isSame(TODAY, 'day');
+    },
+    isYesterday() {
+      const YESTERDAY = REFERENCE.clone().subtract(1, 'days').startOf('day');
+      return momentDate.isSame(YESTERDAY, 'day');
+    },
+    isWithinThisWeek() {
+      const START_OF_WEEK = REFERENCE.clone().startOf('isoWeek');
+      const END_OF_WEEK = REFERENCE.clone().endOf('isoWeek');
+      return momentDate.isBetween(START_OF_WEEK, END_OF_WEEK, 'day', '[]');
+    },
+    isWithinThisMonth() {
+      const START_OF_MONTH = REFERENCE.clone().startOf('month');
+      const END_OF_MONTH = REFERENCE.clone().endOf('month');
+      return momentDate.isBetween(START_OF_MONTH, END_OF_MONTH, 'day', '[]');
+    },
+    isWithinThisYear() {
+      const START_OF_YEAR = REFERENCE.clone().startOf('year');
+      const END_OF_YEAR = REFERENCE.clone().endOf('year');
+      return momentDate.isBetween(START_OF_YEAR, END_OF_YEAR, 'day', '[]');
+    }
+  };
+}
+
+export function checkDateRange(dateToCheck: Date, start: Date, end: Date) {
+  const momentDate = moment(dateToCheck);
+
+  return {
+    isBetweenOrEqual() {
+      return momentDate.isSameOrAfter(start, 'day') && momentDate.isSameOrBefore(end, 'day');
+    }
+  };
+}
+
+type handlePrintPDFType = {
+  pdfDocument: ReactPDF.UsePDFInstance;
+  iframeRef: MutableRefObject<null>;
+};
+
+export const handlePrintPDF = async ({ pdfDocument, iframeRef }: handlePrintPDFType) => {
+  const url = URL.createObjectURL(pdfDocument.blob!);
+
+  if (iframeRef.current) {
+    const iframe: HTMLIFrameElement = iframeRef.current;
+    iframe.src = url;
+    iframe.onload = () => iframe.contentWindow?.print();
+  }
 };
