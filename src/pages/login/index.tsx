7b1e80a23059f6fb86c02890907ab9a23aadbe11
reymondgomera <reymondgomera24@gmail.com>
2023-05-01 14:06:38 +0800

    make login dynamic and implement materialize template loginV1

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,99 +1,36 @@
-import { useState, ReactNode, MouseEvent, ChangeEvent } from 'react';
+import { ReactNode, useState } from 'react';
 
-// ** Next Imports
 import Link from 'next/link';
 
 import {
-  Alert,
+  Box,
   Button,
-  Divider,
   Checkbox,
-  TextField,
-  InputLabel,
-  IconButton,
-  FormControl,
-  useMediaQuery,
-  OutlinedInput,
-  FormHelperText,
-  InputAdornment,
   Grid,
-  CssBaseline,
-  Paper,
-  Avatar
+  Typography,
+  CardContent,
+  styled,
+  useTheme,
+  Card as MuiCard,
+  CardProps
 } from '@mui/material';
-
-import { styled, useTheme } from '@mui/material/styles';
-
-import MuiCard, { CardProps } from '@mui/material/Card';
-
-import Box, { BoxProps } from '@mui/material/Box';
-
-import Typography, { TypographyProps } from '@mui/material/Typography';
 import MuiFormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel';
 
-import Icon from '@/@core/components/icon';
+import { useForm } from 'react-hook-form';
 
-// ** Third Party Imports
-import { useForm, Controller } from 'react-hook-form';
-
-// ** Configs
 import themeConfig from 'src/configs/themeConfig';
-
-// ** Layout Import
 import BlankLayout from 'src/@core/layouts/BlankLayout';
+import FooterIllustrationsV1 from 'src/views/pages/auth/FooterIllustrationsV1';
 
-// ** Demo Imports
-import FooterIllustrationsV2 from 'src/views/pages/auth/FooterIllustrationsV2';
-import { NextPage } from 'next';
 import { signIn, useSession } from 'next-auth/react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/router';
-
-import { LoginUserDtoSchemaType, loginUserDtoSchema } from '@/server/schema/user';
-import { useSettings } from '../../@core/hooks/useSettings';
+import { LoginUserDtoSchemaType, LoginUserFieldType, loginUserDtoSchema } from '@/server/schema/user';
 import { toast } from 'react-hot-toast';
 import { getHomeRoute } from '..';
 
-const LoginIllustrationWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  padding: theme.spacing(20),
-  paddingRight: '0 !important',
-  [theme.breakpoints.down('lg')]: {
-    padding: theme.spacing(10)
-  }
-}));
-
-const LoginIllustration = styled('img')(({ theme }) => ({
-  maxWidth: '38rem',
-  [theme.breakpoints.down('xl')]: {
-    maxWidth: '35rem'
-  },
-  [theme.breakpoints.down('lg')]: {
-    maxWidth: '32rem'
-  }
-}));
-
-const RightWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  width: '100%',
-  [theme.breakpoints.up('md')]: {
-    maxWidth: 400
-  },
-  [theme.breakpoints.up('lg')]: {
-    maxWidth: 450
-  }
-}));
-
-const BoxWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  width: '100%',
-  [theme.breakpoints.down('md')]: {
-    maxWidth: 400
-  }
-}));
-
-const TypographyStyled = styled(Typography)<TypographyProps>(({ theme }) => ({
-  fontWeight: 600,
-  letterSpacing: '0.18px',
-  marginBottom: theme.spacing(1.5),
-  [theme.breakpoints.down('md')]: { marginTop: theme.spacing(8) }
+const Card = styled(MuiCard)<CardProps>(({ theme }) => ({
+  [theme.breakpoints.up('sm')]: { width: 450 }
 }));
 
 const FormControlLabel = styled(MuiFormControlLabel)<FormControlLabelProps>(({ theme }) => ({
@@ -103,16 +40,18 @@ const FormControlLabel = styled(MuiFormControlLabel)<FormControlLabelProps>(({ t
   }
 }));
 
-const LoginPage: NextPage = () => {
-  const theme = useTheme();
-  const { settings } = useSettings();
-  const { skin } = settings;
-  const hidden = useMediaQuery(theme.breakpoints.down('md'));
+import React from 'react';
+import { FormControlPropsType } from '@/utils/common.type';
+import { FormObjectComponent } from '@/utils/form.component';
 
-  const [rememberMe, setRememberMe] = useState<boolean>(true);
-  const [showPassword, setShowPassword] = useState<boolean>(false);
+const LoginPage = () => {
+  const theme = useTheme();
+
   const router = useRouter();
   const { data: session, status } = useSession();
+
+  const [rememberMe, setRememberMe] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
 
   const {
     handleSubmit,
@@ -123,11 +62,35 @@ const LoginPage: NextPage = () => {
       email: '',
       password: ''
     },
-    mode: 'onBlur',
+    mode: 'onChange',
     resolver: zodResolver(loginUserDtoSchema)
   });
 
-  const imageSource = skin === 'bordered' ? 'auth-v2-login-illustration-bordered' : 'auth-v2-login-illustration';
+  const LOGIN_PANEL = ['General'] as const;
+  const LOGIN_FIELDS: Record<(typeof LOGIN_PANEL)[number], FormControlPropsType<LoginUserFieldType>[]> = {
+    General: [
+      {
+        label: 'Email',
+        dbField: 'email',
+        type: 'textField',
+        required: true,
+        autoFocus: true,
+        extendedProps: {
+          gridAttribute: { xs: 12 }
+        }
+      },
+      {
+        label: 'Password',
+        dbField: 'password',
+        type: 'textField',
+        required: true,
+        extendedProps: {
+          gridAttribute: { xs: 12 },
+          textFieldAttribute: { type: 'password' }
+        }
+      }
+    ]
+  };
 
   const onSubmit = async (data: LoginUserDtoSchemaType) => {
     try {
@@ -146,207 +109,133 @@ const LoginPage: NextPage = () => {
   if (status === 'authenticated') router.replace(getHomeRoute(session.user.role.code));
 
   return (
-    <Box className='content-right'>
-      {!hidden ? (
-        <Box
-          sx={{
-            flex: 1,
-            display: 'flex',
-            position: 'relative',
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}
-        >
-          <LoginIllustrationWrapper>
-            <LoginIllustration
-              alt='login-illustration'
-              src={`/images/pages/${imageSource}-${theme.palette.mode}.png`}
-            />
-          </LoginIllustrationWrapper>
-          <FooterIllustrationsV2 />
-        </Box>
-      ) : null}
-      <RightWrapper sx={skin === 'bordered' && !hidden ? { borderLeft: `1px solid ${theme.palette.divider}` } : {}}>
-        <Box
-          sx={{
-            p: 7,
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'background.paper'
-          }}
-        >
-          <BoxWrapper>
+    <Box className='content-center'>
+      <Card sx={{ zIndex: 1 }}>
+        <CardContent sx={{ p: theme => `${theme.spacing(13, 7, 6.5)} !important` }}>
+          <Box sx={{ mb: 8, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <svg width={47} fill='none' height={26} viewBox='0 0 268 150' xmlns='http://www.w3.org/2000/svg'>
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 195.571 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fillOpacity='0.4'
+                fill='url(#paint0_linear_7821_79167)'
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 196.084 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(0.865206 0.501417 -0.498585 0.866841 173.147 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fillOpacity='0.4'
+                fill='url(#paint1_linear_7821_79167)'
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(0.865206 0.501417 -0.498585 0.866841 71.7728 0)'
+              />
+              <defs>
+                <linearGradient
+                  y1='0'
+                  x1='25.1443'
+                  x2='25.1443'
+                  y2='143.953'
+                  id='paint0_linear_7821_79167'
+                  gradientUnits='userSpaceOnUse'
+                >
+                  <stop />
+                  <stop offset='1' stopOpacity='0' />
+                </linearGradient>
+                <linearGradient
+                  y1='0'
+                  x1='25.1443'
+                  x2='25.1443'
+                  y2='143.953'
+                  id='paint1_linear_7821_79167'
+                  gradientUnits='userSpaceOnUse'
+                >
+                  <stop />
+                  <stop offset='1' stopOpacity='0' />
+                </linearGradient>
+              </defs>
+            </svg>
+            <Typography variant='h6' sx={{ ml: 2, lineHeight: 1, fontWeight: 700, fontSize: '1.5rem !important' }}>
+              {themeConfig.templateName}
+            </Typography>
+          </Box>
+          <Box sx={{ mb: 6 }}>
+            <Typography variant='h5' sx={{ mb: 1.5, fontWeight: 600, letterSpacing: '0.18px' }}>
+              {`Welcome to ${themeConfig.templateName}! 👋🏻`}
+            </Typography>
+            <Typography variant='body2'>Please enter your credentials.</Typography>
+          </Box>
+
+          <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+            <Grid container rowGap={3}>
+              {LOGIN_FIELDS['General'].map((obj, i) => (
+                <Grid item key={obj.dbField} {...obj.extendedProps?.gridAttribute}>
+                  <FormObjectComponent key={i} objFieldProp={obj} control={control} errors={errors} />
+                </Grid>
+              ))}
+            </Grid>
+
             <Box
               sx={{
-                top: 30,
-                left: 40,
+                mb: 4,
                 display: 'flex',
-                position: 'absolute',
                 alignItems: 'center',
-                justifyContent: 'center'
+                flexWrap: 'wrap',
+                justifyContent: 'space-between'
               }}
             >
-              <svg width={47} fill='none' height={26} viewBox='0 0 268 150' xmlns='http://www.w3.org/2000/svg'>
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 195.571 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fillOpacity='0.4'
-                  fill='url(#paint0_linear_7821_79167)'
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 196.084 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(0.865206 0.501417 -0.498585 0.866841 173.147 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fillOpacity='0.4'
-                  fill='url(#paint1_linear_7821_79167)'
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(0.865206 0.501417 -0.498585 0.866841 71.7728 0)'
-                />
-                <defs>
-                  <linearGradient
-                    y1='0'
-                    x1='25.1443'
-                    x2='25.1443'
-                    y2='143.953'
-                    id='paint0_linear_7821_79167'
-                    gradientUnits='userSpaceOnUse'
-                  >
-                    <stop />
-                    <stop offset='1' stopOpacity='0' />
-                  </linearGradient>
-                  <linearGradient
-                    y1='0'
-                    x1='25.1443'
-                    x2='25.1443'
-                    y2='143.953'
-                    id='paint1_linear_7821_79167'
-                    gradientUnits='userSpaceOnUse'
-                  >
-                    <stop />
-                    <stop offset='1' stopOpacity='0' />
-                  </linearGradient>
-                </defs>
-              </svg>
+              <FormControlLabel
+                label='Remember Me'
+                control={<Checkbox />}
+                sx={{
+                  '& .MuiFormControlLabel-label': { color: 'text.primary' }
+                }}
+              />
               <Typography
-                variant='h6'
-                sx={{
-                  ml: 2,
-                  lineHeight: 1,
-                  fontWeight: 700,
-                  fontSize: '1.5rem !important'
-                }}
+                variant='body2'
+                component={Link}
+                href='/pages/auth/forgot-password-v2'
+                sx={{ color: 'primary.main', textDecoration: 'none' }}
               >
-                {themeConfig.templateName}
-              </Typography>
-            </Box>
-            <Box sx={{ mb: 6 }}>
-              <TypographyStyled
-                variant='h5'
-                textAlign='center'
-              >{`Welcome to ${themeConfig.templateName}! 👋🏻`}</TypographyStyled>
-              <Typography variant='body2' textAlign='center'>
-                Please enter your credentials.
+                Forgot Password?
               </Typography>
             </Box>
 
-            <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
-              <FormControl fullWidth sx={{ mb: 4 }}>
-                <Controller
-                  name='email'
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      name='email'
-                      label='Email'
-                      error={Boolean(errors.email)}
-                      helperText={errors.email?.message}
-                    />
-                  )}
-                />
-              </FormControl>
-
-              <FormControl fullWidth sx={{ mb: 4 }}>
-                <Controller
-                  name='password'
-                  control={control}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      type='password'
-                      name='password'
-                      label='Password'
-                      error={Boolean(errors.password)}
-                      helperText={errors.password?.message}
-                    />
-                  )}
-                />
-              </FormControl>
-
-              <Box
-                sx={{
-                  mb: 4,
-                  display: 'flex',
-                  alignItems: 'center',
-                  flexWrap: 'wrap',
-                  justifyContent: 'space-between'
-                }}
-              >
-                <FormControlLabel
-                  label='Remember Me'
-                  control={<Checkbox />}
-                  sx={{
-                    '& .MuiFormControlLabel-label': { color: 'text.primary' }
-                  }}
-                />
-                <Typography
-                  variant='body2'
-                  component={Link}
-                  href='/pages/auth/forgot-password-v2'
-                  sx={{ color: 'primary.main', textDecoration: 'none' }}
-                >
-                  Forgot Password?
-                </Typography>
-              </Box>
-              <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 7 }}>
-                Login
-              </Button>
-            </form>
-          </BoxWrapper>
-        </Box>
-      </RightWrapper>
+            <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 7 }}>
+              Login
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+      <FooterIllustrationsV1 />
     </Box>
   );
 };
