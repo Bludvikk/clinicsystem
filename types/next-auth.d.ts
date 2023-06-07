cf5fee187b1ce29a5db9ecd05ed15cdaf690a682
reymondgomera <reymondgomera24@gmail.com>
2023-06-07 09:29:39 +0800

    add choose clinic page & functionalities, modify auth & requireAuth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,20 +1,25 @@
+import type { User } from 'next-auth';
 import { UsersType } from '@/utils/db.type';
 import NextAuth from 'next-auth';
 
-export type AuthenticatedUserType = Pick<UsersType, 'id' | 'email' | 'userName' | 'role' | 'department' | 'status'>;
+export type AuthenticatedUserType = Pick<
+  UsersType,
+  'id' | 'email' | 'userName' | 'role' | 'department' | 'status' | 'profile'
+>;
 
 declare module 'next-auth' {
   interface Session {
-    user: AuthenticatedUserType;
+    user: User;
   }
 
   interface User extends AuthenticatedUserType {
     id: number;
+    clinicId?: number;
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
-    user: AuthenticatedUserType;
+    user: User;
   }
 }
