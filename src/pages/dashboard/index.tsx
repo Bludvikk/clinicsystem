80eafe252e87a0ddac9bd9e27f6f0e881782c3a5
Bludvikk <cleverdmasoy@gmail.com>
2023-06-16 15:02:48 +0800

    test

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+import { NextPage, InferGetServerSidePropsType } from "next";
 import { useSession, signOut } from "next-auth/react";
 
 import Box from "@mui/material/Box";
@@ -7,14 +7,31 @@ import { requireAuth } from "@/common/requireAuth";
 import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
 import PatientInfoForm from "@/views/pages/patient/PatientInfoForm";
 import { FormPropsType } from "@/utils/common.type";
-import AddMedicationForm from "@/views/pages/patient/addMedication";
+import { useEffect, useState } from "react";
+// import AddMedicationForm from "@/views/pages/patient/addMedication";
 
 export const getServerSideProps = requireAuth(async () => {
   return { props: {} };
 });
 
-const Dashboard = ({formId}: FormPropsType) => {
+const Dashboard = ({formId}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+
   const { data } = useSession();
+  const [xx, setXX] = useState()
+  console.log(xx);
+
+  useEffect( () => {
+    const fetchData = async () => {
+      const result = await fetch('https://sentry-testdpy.vercel.app/api/trpc/sales', { method: 'GET' })
+      const json = await result.json()
+
+      setXX(json)
+     }  
+
+    fetchData().then(data => console.log(data)).catch(err => console.log(err))
+    // const body =  result.json()
+  }, [])
+
 
   return (
     <Grid container>
